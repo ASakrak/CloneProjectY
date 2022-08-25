c39c015c6f1ab6b37a630c8065c09b8e43f49047
LnxyBTW <TTV_BTW73498@outlook.com>
2022-08-26 01:03:24 +0300

    update

---
+++ cloneproject_x/src/components/Dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="menu-item">
+        <a href="#">
+            {{ title }}
+        </a>
+    </div>
+
+</template>
+
+
+<script>
+export default {
+    name: 'dropdownVue',
+    props: ['title', 'items']
+}
+</script>
+
+
+<style>
+</style>
---
+++ cloneproject_x/src/components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+    <nav>
+        <div class="menu-item"><a href="#">Blizzard</a></div>
+        <div class="menu-item"><a href="#">Warcraft</a></div>
+        <div class="menu-item"><a href="#">Diablo</a></div>
+        <div class="menu-item"><a href="#">Overwatch</a></div>
+        <div class="menu-item"><a href="#">StarCraft</a></div>
+        <Dropdown title="Test" :items="Test" />
+        <div class="menu-item"><a href="#">...</a></div>
+    </nav>
+</template>
+
+<script>
+import Dropdown from './Dropdown.vue'
+
+
+
+export default {
+    name: "navbarVue",
+    components: {
+        Dropdown
+    },
+    data() {
+        return {
+            Test: [
+                {
+                    title: "first",
+                    link: "#"
+                },
+                {
+                    title: "second",
+                    link: "#"
+                },
+                {
+                    title: "third",
+                    link: "#"
+                }
+            ]
+        };
+    },
+    components: { Dropdown }
+}
+</script>
+
+<style>
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+nav .menu-items {
+    color: #fff;
+    padding: 10px 20px;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid transparent;
+    display: flex;
+    transition: 0.4s;
+
+}
+
+nav .menu-item.active,
+nav .menu-item:hover {
+    background-color: rgb(0, 0, 0);
+    border-bottom-color: rgb(165, 165, 165);
+}
+
+nav .menu-item a {
+    color: inherit;
+    text-decoration: none;
+
+}
+</style>
